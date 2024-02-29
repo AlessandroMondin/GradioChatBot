9285960f9d54dad80bf1648277a619249fbe88b8
AlessandroMondin <alessandromondin00@gmail.com>
2024-02-29 11:53:01 +0100

    Streaming chat with Server-Sent-Events

--- src/app.py
+++ src/app.py
@@ -3,11 +3,14 @@ from typing import Union, List
 
 from dotenv import load_dotenv
 from fastapi import FastAPI
+from fastapi.responses import StreamingResponse
 from langchain.chat_models import ChatOpenAI
 from langchain.schema import AIMessage, HumanMessage
 from pydantic import BaseModel
 
 load_dotenv()
+
+END_TOKEN = os.environ.get("END_TOKEN", "1234ASDFzxcv")
 
 app = FastAPI()
 llm = ChatOpenAI(temperature=1.0, model="gpt-3.5-turbo-0613")
@@ -25,8 +28,12 @@ async def reply(file: RequestData):
         history_langchain_format.append(HumanMessage(content=human))
         history_langchain_format.append(AIMessage(content=ai))
     history_langchain_format.append(HumanMessage(content=file.message))
-    gpt_response = llm(history_langchain_format).content
 
-    return {
-        "content": gpt_response,
-    }
+    # https://python.langchain.com/docs/expression_language/streaming
+    async def stream_response():
+        async for chunk in llm.astream(history_langchain_format):
+            yield chunk.content
+        yield END_TOKEN
+
+    # set media_type="text/event-stream" because of https://stackoverflow.com/a/75760884
+    return StreamingResponse(stream_response(), media_type="text/event-stream")
--- src/interface.py
+++ src/interface.py
@@ -1,19 +1,33 @@
-import ast
+import os
+
+from dotenv import load_dotenv
 import gradio as gr
 import requests
+
+load_dotenv()
+
+END_TOKEN = os.environ.get("END_TOKEN", "1234ASDFzxcv")
 
 
 def predict(message, history):
     # Assuming your FastAPI endpoint is set up to receive a POST request at /predict
     url = "http://localhost:8000/predict/"
     data = {"message": message, "history": history}
-    response = requests.post(url, json=data)
 
-    if response.status_code == 200:
-        content = ast.literal_eval(response.content.decode("utf-8"))["content"]
-        return content
-    else:
-        return "Error: Could not get a response from the FastAPI backend."
+    response = ""
+    s = requests.Session()
+    with s.post(url, headers=None, json=data, stream=True) as r:
+        # https://requests.readthedocs.io/en/latest/api/
+        # "A value of None will function differently depending on the value of
+        # stream. stream=True will read data as it arrives in whatever size the
+        #  chunks are received"
+        for chunk in r.iter_content(chunk_size=None):
+            chunk = chunk.decode("utf-8")
+            if chunk != END_TOKEN:
+                response += chunk
+                yield response
+            else:
+                break
 
 
 if __name__ == "__main__":
