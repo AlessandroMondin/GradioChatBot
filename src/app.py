de7733e79b52bee1fd75059870cae323ed65e525
AlessandroMondin <alessandromondin00@gmail.com>
2024-02-29 11:00:14 +0100

    Pipeline with Server-Sent-Events

--- src/app.py
+++ src/app.py
@@ -1,52 +1,39 @@
-from typing import List
-from fastapi import FastAPI, WebSocket
+import os
+from typing import Union, List
+
 from dotenv import load_dotenv
+from fastapi import FastAPI
+from fastapi.responses import StreamingResponse
 from langchain.chat_models import ChatOpenAI
 from langchain.schema import AIMessage, HumanMessage
+from pydantic import BaseModel
 
 load_dotenv()
 
+END_TOKEN = os.environ.get("END_TOKEN", "1234ASDFzxcv")
+
 app = FastAPI()
-llm = ChatOpenAI(temperature=1.0, model="gpt-3.5-turbo-0613", streaming=True)
+llm = ChatOpenAI(temperature=1.0, model="gpt-3.5-turbo-0613")
 
 
-class ConnectionManager:
-    def __init__(self):
-        self.active_connections: List[WebSocket] = []
-
-    async def connect(self, websocket: WebSocket):
-        await websocket.accept()
-        self.active_connections.append(websocket)
-
-    def disconnect(self, websocket: WebSocket):
-        self.active_connections.remove(websocket)
-
-    async def send_personal_message(self, message: str, websocket: WebSocket):
-        await websocket.send_text(message)
+class RequestData(BaseModel):
+    message: str
+    history: Union[List[List], List]
 
 
-manager = ConnectionManager()
+@app.post("/predict")
+async def reply(file: RequestData):
+    history_langchain_format = []
+    for human, ai in file.history:
+        history_langchain_format.append(HumanMessage(content=human))
+        history_langchain_format.append(AIMessage(content=ai))
+    history_langchain_format.append(HumanMessage(content=file.message))
 
+    # https://python.langchain.com/docs/expression_language/streaming
+    async def stream_response():
+        async for chunk in llm.astream(history_langchain_format):
+            yield chunk.content
+        yield END_TOKEN
 
-@app.websocket("/ws/chat")
-async def websocket_endpoint(websocket: WebSocket):
-    await manager.connect(websocket)
-    try:
-        while True:
-            data = await websocket.receive_json()
-            message = data["message"]
-            history = data.get("history", [])
-
-            # Process the message through the language model as done in the /predict endpoint
-            history_langchain_format = []
-            for human, ai in history:
-                history_langchain_format.append(HumanMessage(content=human))
-                history_langchain_format.append(AIMessage(content=ai))
-            history_langchain_format.append(HumanMessage(content=message))
-            for chuck in llm.stream(history_langchain_format):
-                await manager.send_personal_message(chuck.content, websocket)
-            await manager.send_personal_message("<generation_completed>", websocket)
-    except Exception as e:
-        print(f"Error: {e}")
-    finally:
-        manager.disconnect(websocket)
+    # set media_type="text/event-stream" because of https://stackoverflow.com/a/75760884
+    return StreamingResponse(stream_response(), media_type="text/event-stream")
--- src/interface.py
+++ src/interface.py
@@ -1,48 +1,44 @@
-import websockets
-import json
+import os
 
+from dotenv import load_dotenv
 import gradio as gr
+import requests
+
+load_dotenv()
+
+END_TOKEN = os.environ.get("END_TOKEN", "1234ASDFzxcv")
 
 
-class ChatStorage:
-    def __init__(self, websocket_url="ws://localhost:8000/ws/chat") -> None:
+class ChatInterface:
+    def __init__(self) -> None:
+
         self.history = []
-        self.websocket_url = websocket_url
-        self.websocket = None
 
-    async def connect_to_websocket(self):
-        if self.websocket is None or self.websocket.closed:
-            self.websocket = await websockets.connect(self.websocket_url)
+    def predict(self, message, history):
+        # Assuming your FastAPI endpoint is set up to receive a POST request at /predict
+        url = "http://localhost:8000/predict/"
+        data = {"message": message, "history": self.history}
 
-    async def send_and_receive_messages(self, message, history):
-        # Prepare the data as per the original predict method's requirement
-        await self.connect_to_websocket()
-        data = {"message": message, "history": history}
-
-        # convert the Python dict into JSON object.
-        data_json = json.dumps(data)
-
-        await self.websocket.send(data_json)
-        # As 27/02/2024, gr.ChatInterface does not allow passing
-        # via streaming the one chuck at a time. So we accumulate
-        # chucks while receiving them.
         response = ""
-        # Asynchronously iterating over the websocket responses.
-        async for chunk in self.websocket:
-            # If the token is not the end token `<generation_completed>`, then we are
-            # continuing out stream into gradio UI.
-            if chunk != "<generation_completed>":
-                response += chunk
-                yield response
-            else:
-                break
+        s = requests.Session()
+        with s.post(url, headers=None, json=data, stream=True) as r:
+            # https://requests.readthedocs.io/en/latest/api/
+            # "A value of None will function differently depending on the value of
+            # stream. stream=True will read data as it arrives in whatever size the
+            #  chunks are received"
+            for chunk in r.iter_content(chunk_size=None):
+                chunk = chunk.decode("utf-8")
+                if chunk != END_TOKEN:
+                    response += chunk
+                    yield response
+                else:
+                    break
 
-        # Update history after once the response is completed.
         self.history.append([message, response])
 
 
 if __name__ == "__main__":
-    # initilise chatstorage object used to store previous messages.
-    chat = ChatStorage()
-    app = gr.ChatInterface(fn=chat.send_and_receive_messages)
-    app.launch(share=True)
+    chat = ChatInterface()
+    gr.ChatInterface(
+        fn=chat.predict,
+    ).launch()
